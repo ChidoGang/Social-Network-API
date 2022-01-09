4760ecf9171a4730f26fe7c5f9a4a40081b3ca9e
ChidoGang <edgar_quintanilla@mymail.rcbc.edu>
2022-01-09 17:47:14 -0500

    fixed error connected local host

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app =express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT|| 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network-api', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Social-Network-Api', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
